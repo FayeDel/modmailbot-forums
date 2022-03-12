557ba943f50bd91aca3a724c246f97d5df114aaa
Ravy <ravy@farfrom.earth>
2022-03-12 05:09:19 +0100

    feat: initialize db connection, limit eval command to owners, add ModMail client struct

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -2,6 +2,11 @@
 
 const { SlashCommand, CommandOptionType } = require('slash-create');
 const util = require('~/lib/util');
+const constants = require('~/lib/constants');
+const { inspect } = require('util');
+
+/* eslint-disable-next-line no-unused-vars */
+const { Routes } = require('discord-api-types/v10');
 
 module.exports = class SetupCommand extends SlashCommand {
 
@@ -9,25 +14,42 @@ module.exports = class SetupCommand extends SlashCommand {
 		super(creator, {
 			name: 'eval',
 			description: 'Runs code for testing purposes.',
-			options: [{
-				type: CommandOptionType.STRING,
-				name: 'code',
-				description: 'The code to execute.',
-				required: true
-			}]
+			options: [
+				{
+					type: CommandOptionType.STRING,
+					name: 'code',
+					description: 'The code to execute.',
+					required: true
+				},
+				{
+					type: CommandOptionType.INTEGER,
+					name: 'depth',
+					description: 'How far to evaluate properties.',
+					required: false
+				}
+			],
+			guildIDs: [constants.RYDIXORD]
 		});
 		this.filePath = __filename;
 	}
 
 	async run(ctx) {
+		if (!ctx.creator.client.owners.includes(ctx.user.id)) return `You may not run the eval command`;
+
 		let { code } = ctx.options;
+		const { depth } = ctx.options;
+
 		code = `(async () => {\n${code}\n})();`;
 
 		let result = eval(code);
 
 		if (util.isThenable(result)) result = await result;
 
-		return ctx.reply(util.codeBlock(result, 'js'));
+		result = inspect(result, {
+			depth: depth || 0
+		});
+
+		return util.codeBlock(result, 'js');
 	}
 
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 require('@aero/require').config(process.cwd(), false);
 
-const constants = require('~/lib/constants');
+const ModMail = require('~/lib/ModMail');
+const { MongoClient } = require('mongodb');
+const mc = new MongoClient(process.env.MONGO_URI);
 
 const { ExpressServer, SlashCreator } = require('slash-create');
-// const { RestClient } = require("@discordjs/rest");
 
 // logger instance because its better to distinguish than pure console.log()
 const CatLoggr = require('cat-loggr');
@@ -12,36 +13,45 @@ const logger = new CatLoggr().setLevel(process.env.COMMANDS_DEBUG === 'true' ? '
 
 const path = require('path');
 
-const creator = new SlashCreator({
-	applicationID: process.env.DISCORD_APP_ID,
-	publicKey: process.env.DISCORD_PUBLIC_KEY,
-	token: process.env.DISCORD_BOT_TOKEN,
-	serverPort: 5050
-});
-
-creator.on('debug', (message) => logger.log(message));
-creator.on('warn', (message) => logger.warn(message));
-creator.on('error', (error) => logger.error(error));
-creator.on('synced', () => logger.info('Commands synced!'));
-creator.on('commandRun', (command, _, ctx) =>
-	logger.info(`${ctx.user.username}#${ctx.user.discriminator} (${ctx.user.id}) ran command ${command.commandName}`));
-creator.on('commandRegister', (command) =>
-	logger.info(`Registered command ${command.commandName}`));
-creator.on('commandError', (command, error) => logger.error(`Command ${command.commandName}:`, error));
-
 /*
  * The slash command client object will listen on port 5050.
  * For local development, ExpressServer's fine.
  * If this were to scale, you may need to use something else that `slash-create` supports.
  */
 
-creator.withServer(new ExpressServer())
-	.registerCommandsIn(path.join(__dirname, 'commands'))
-	.syncCommands()
-	.startServer();
+async function main() {
+	await mc.connect();
 
-creator
-	.registerCommandsIn(path.join(__dirname, 'commands'))
-	.syncCommandsIn(constants.RYDIXORD);
+	const client = new ModMail(mc.db(process.env.MONGO_DB), process.env.DISCORD_BOT_TOKEN);
 
-console.log(`Starting server at "localhost:${creator.options.serverPort}/interactions"`);
+	await client.fetchOwners();
+
+	const creator = new SlashCreator({
+		applicationID: process.env.DISCORD_APP_ID,
+		publicKey: process.env.DISCORD_PUBLIC_KEY,
+		token: process.env.DISCORD_BOT_TOKEN,
+		serverPort: 5050,
+		client
+	});
+
+	creator.on('debug', (message) => logger.log(message));
+	creator.on('warn', (message) => logger.warn(message));
+	creator.on('error', (error) => logger.error(error));
+	creator.on('synced', () => logger.info('Commands synced!'));
+	creator.on('commandRun', (command, _, ctx) =>
+		logger.info(`${ctx.user.username}#${ctx.user.discriminator} (${ctx.user.id}) ran command ${command.commandName}`));
+	creator.on('commandRegister', (command) =>
+		logger.info(`Registered command ${command.commandName}`));
+	creator.on('commandError', (command, error) => logger.error(`Command ${command.commandName}:`, error));
+
+	creator.withServer(new ExpressServer())
+		.registerCommandsIn(path.join(__dirname, 'commands'))
+		.syncCommands()
+		.startServer();
+
+	return creator;
+}
+
+main().then((creator) => {
+	logger.info(`Starting server at localhost:${creator.options.serverPort}/interactions`);
+});
