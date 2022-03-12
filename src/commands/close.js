1a91b6c10665103d8ddaa5e87bafe37857199588
Ravy <ravy@farfrom.earth>
2022-03-12 07:04:03 +0100

    feat: add prechecks to close

--- src/commands/close.js
+++ src/commands/close.js
@@ -1,6 +1,8 @@
 const { SlashCommand } = require('slash-create');
 const util = require('~/lib/util');
 
+const { success, error } = require('~/lib/constants').emoji;
+
 module.exports = class CloseCommand extends SlashCommand {
 
 	constructor(creator) {
@@ -13,9 +15,14 @@ module.exports = class CloseCommand extends SlashCommand {
 
 	async run(ctx) {
 		const channel = await ctx.creator.client.guild(ctx.guildID).channel(ctx.channelID);
-		if (util.isThread(channel)) return `isThread`;
+		if (!util.isThread(channel)) return `${error} This channel isn't a thread. Try going to <#${forumChannelID}> and selecting a thread there.`;
 
-		return 'is not thread';
+		const forumChannelID = await ctx.creator.client.guild(ctx.guildID).getForumChannel();
+		if (!forumChannelID) return `${error} The bot hasn't been set up yet. Try running \`/setup\`.`;
+
+		if (channel.parent_id !== forumChannelID) return `${error} This isn't the channel that has been set up for tickets. Try going to <#${forumChannelID}> or running \`/setup\`.`;
+
+		return success;
 	}
 
 };
